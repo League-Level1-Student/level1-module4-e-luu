83ad20d7c967201c895fb7169071cb2dcf7bc2e4
League Student <league@iMac-18.attlocal.net>
2019-07-12 16:26:39 -0700

    typing tutor, book of illusions, and fortune teller

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,17 +1,65 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class TypingTutor {
-JFrame frame = new JFrame();
-public static current {
-char currentLetter;
-currentLetter = generateRandomLetter();
+public class TypingTutor implements KeyListener {
+	JFrame f = new JFrame();
+	JLabel l = new JLabel();
+	char currentLetter;
+	static TypingTutor t = new TypingTutor();
 
-char generateRandomLetter() {
-    Random r = new Random();
-    return (char) (r.nextInt(26) + 'a');
-}
+	public void current() {
+		currentLetter = generateRandomLetter();
+
+		f.setVisible(true);
+		f.add(l);
+		l.setText(currentLetter + "");
+		l.setFont(l.getFont().deriveFont(28.0f));
+		l.setHorizontalAlignment(JLabel.CENTER);
+		f.addKeyListener(this);
+		f.pack();
+	}
+
+	static char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+
+	public static void main(String[] args) {
+
+		t.current();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("you typed: " + e.getKeyChar());
+		if (e.getKeyChar() == currentLetter) {
+			System.out.println("correct");
+			f.setBackground(Color.GREEN);
+			f.pack();
+		} else {
+			f.setBackground(Color.RED);
+			f.pack();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = t.generateRandomLetter();
+		l.setText(currentLetter + "");
+	}
 }
--- src/_07_fortune_teller/FortuneTeller.java
+++ src/_07_fortune_teller/FortuneTeller.java
@@ -33,7 +33,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
      // 3. Complete the begin() method in the FortuneTellerRunner class
  	 // 4. add a mouse listener to the frame
-
+frame.addMouseListener(this);
     }
 
 
@@ -43,7 +43,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-
+System.out.println(mouseX +", " + mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
    	 int secretLocationX = 0;
